d0e65d5e9084d57caf0cf19100102b23f0d6fe76
ilkek <ilkerozguraslan@gmail.com>
2025-11-27 19:52:21 +0100

    fix: layout

--- transaction/src/commonMain/kotlin/me/ilker/transaction/transactions/views/TransactionsLoadedView.kt
+++ transaction/src/commonMain/kotlin/me/ilker/transaction/transactions/views/TransactionsLoadedView.kt
@@ -1,20 +1,21 @@
 package me.ilker.transaction.transactions.views
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import me.ilker.balance_tracker.resources.Res
+import me.ilker.balance_tracker.resources.add
 import me.ilker.balance_tracker.resources.app_name
 import me.ilker.transaction.transactions.TransactionDomainModel
 import org.jetbrains.compose.resources.stringResource
@@ -25,21 +26,30 @@ internal fun TransactionsLoadedView(
     add: () -> Unit
 ) {
     Scaffold(
-        modifier = Modifier,
+        modifier = Modifier.padding(12.dp),
         topBar = {
-
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Text(
+                    modifier = Modifier.padding(12.dp),
+                    text = stringResource(Res.string.app_name)
+                )
+            }
         },
         bottomBar = {
-            IconButton(
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 12.dp, vertical = 16.dp),
                 onClick = add,
                 content = {
-                    Text(stringResource(Res.string.app_name))
+                    Text(stringResource(Res.string.add))
                 }
             )
         }
-    ) {
+    ) { paddingValues ->
         LazyColumn(
             modifier = Modifier
+                .padding(paddingValues)
                 .background(MaterialTheme.colorScheme.onError)
         ) {
             items(transactions) { transaction ->
@@ -52,7 +62,6 @@ internal fun TransactionsLoadedView(
                         text = transaction.amount.toString()
                     )
                 }
-
             }
         }
     }
