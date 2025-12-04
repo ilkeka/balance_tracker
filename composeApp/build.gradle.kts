c3be11e382fb1774533b8a1759bc5862256dc9a5
ilkek <ilkerozguraslan@gmail.com>
2025-12-04 15:16:22 +0100

    fix: colors

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "me.ilker.balance_tracker"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
     packaging {
         resources {
