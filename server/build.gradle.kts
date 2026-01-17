c51054ee768fb4d6b7a2115ce840a2937be6b25c
ilkek <ilkerozguraslan@gmail.com>
2026-01-17 19:14:08 +0100

    feat: update host

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -11,9 +11,6 @@ version = "1.0.0"
 
 application {
     mainClass.set("me.ilker.balance_tracker.ApplicationKt")
-    
-    val isDevelopment: Boolean = project.ext.has("development")
-    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
 ktor {
@@ -32,6 +29,7 @@ dependencies {
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.ktor.serialization.json)
     implementation(libs.ktor.server.auth)
+    implementation(libs.ktor.server.config.yaml)
     implementation(libs.ktor.server.content.negotiation)
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.cors)
