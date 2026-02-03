e18e8d3f292854286b61fcb351076b2d3cc18d52
ilkek <ilkerozguraslan@gmail.com>
2026-02-03 17:42:23 +0100

    feat: add desktop target

--- shared/src/jvmMain/kotlin/me/ilker/balance_tracker/database/DatabaseDriverFactory.jvm.kt
+++ shared/src/jvmMain/kotlin/me/ilker/balance_tracker/database/DatabaseDriverFactory.jvm.kt
@@ -1,16 +1,13 @@
 package me.ilker.balance_tracker.database
 
+import app.cash.sqldelight.async.coroutines.synchronous
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
 import me.ilker.balance_tracker.Database
 
 actual class DatabaseDriverFactory {
-    actual fun createDriver(): SqlDriver {
-        val driver = JdbcSqliteDriver(url = "jdbc:sqlite:Database.db")
-            .also {
-                Database.Schema.create(it)
-            }
-
-        return driver
-    }
+    actual fun createDriver(): SqlDriver = JdbcSqliteDriver(
+        url = "jdbc:sqlite:Database.db",
+        schema = Database.Schema.synchronous()
+    )
 }
