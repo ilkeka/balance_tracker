98b8e71d6f641581370a0e89b46f46b605f9b994
ilkek <ilkerozguraslan@gmail.com>
2026-02-03 11:07:51 +0100

    chore: target web

--- server/src/main/kotlin/me/ilker/balance_tracker/ServerModule.kt
+++ server/src/main/kotlin/me/ilker/balance_tracker/ServerModule.kt
@@ -1,13 +1,14 @@
 package me.ilker.balance_tracker
 
 import me.ilker.balance_tracker.database.DB
+import me.ilker.balance_tracker.database.DatabaseDriverFactory
 import me.ilker.balance_tracker.database.impl.DBImpl
 import org.koin.dsl.module
 
 internal val serverModule = module {
     single<DB> {
         DBImpl(
-            databaseDriverFactory = DatabaseDriverFactoryImpl()
+            databaseDriverFactory = DatabaseDriverFactory()
         )
     }
 }
