6b50ebcbfe0aa42645852edab00225aaba7e8efd
ilkek <ilkerozguraslan@gmail.com>
2026-02-03 11:47:39 +0100

    chore: bump version to 1.0.4

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "me.ilker.balance_tracker"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
     }
     packaging {
         resources {
