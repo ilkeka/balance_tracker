1b762e58f928a12b26ab22583b830c2d22dcf639
ilkek <ilkerozguraslan@gmail.com>
2026-02-22 19:16:06 +0100

    feat: show detail fields

--- shared/transaction/src/commonMain/kotlin/me/ilker/transaction/details/views/TransactionDetailsLoadedView.kt
+++ shared/transaction/src/commonMain/kotlin/me/ilker/transaction/details/views/TransactionDetailsLoadedView.kt
@@ -1,14 +1,20 @@
 package me.ilker.transaction.details.views
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,7 +25,13 @@ import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import me.ilker.balance_tracker.resources.Res
+import me.ilker.balance_tracker.resources.amount
+import me.ilker.balance_tracker.resources.date
+import me.ilker.balance_tracker.resources.expense
+import me.ilker.balance_tracker.resources.income
 import me.ilker.balance_tracker.resources.transaction_details
+import me.ilker.balance_tracker.resources.transaction_type
+import me.ilker.balance_tracker.sdk.TransactionType
 import me.ilker.transaction.details.TransactionDetailsState
 import org.jetbrains.compose.resources.stringResource
 
@@ -63,11 +75,68 @@ internal fun TransactionDetailsLoadedView(
             verticalArrangement = Arrangement.spacedBy(12.dp)
         ) {
             item {
-                Text(
-                    text = state.transaction.amount.toString(),
-                    fontSize = TextUnit(value = 16f, type = TextUnitType.Sp),
-                    fontWeight = FontWeight.Normal
-                )
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp)
+                        .background(
+                            color = MaterialTheme.colorScheme.secondaryContainer,
+                            shape = RoundedCornerShape(8.dp)
+                        )
+                        .padding(
+                            horizontal = 16.dp,
+                            vertical = 8.dp
+                        )
+                ) {
+                    Text(
+                        text = stringResource(Res.string.amount),
+                        fontSize = TextUnit(value = 16f, type = TextUnitType.Sp),
+                        fontWeight = FontWeight.SemiBold,
+                        color = MaterialTheme.colorScheme.secondary
+                    )
+
+                    Text(
+                        text = state.transaction.amount.toString(),
+                        fontSize = TextUnit(value = 16f, type = TextUnitType.Sp),
+                        fontWeight = FontWeight.Normal,
+                        color = MaterialTheme.colorScheme.secondary
+                    )
+
+                    Spacer(Modifier.height(16.dp))
+
+                    Text(
+                        text = stringResource(Res.string.transaction_type),
+                        fontSize = TextUnit(value = 16f, type = TextUnitType.Sp),
+                        fontWeight = FontWeight.SemiBold,
+                        color = MaterialTheme.colorScheme.secondary
+                    )
+
+                    Text(
+                        text = when (state.transaction.type) {
+                            TransactionType.Expense -> stringResource(Res.string.expense)
+                            TransactionType.Income -> stringResource(Res.string.income)
+                        },
+                        fontSize = TextUnit(value = 16f, type = TextUnitType.Sp),
+                        fontWeight = FontWeight.Normal,
+                        color = MaterialTheme.colorScheme.secondary
+                    )
+
+                    Spacer(Modifier.height(16.dp))
+
+                    Text(
+                        text = stringResource(Res.string.date),
+                        fontSize = TextUnit(value = 16f, type = TextUnitType.Sp),
+                        fontWeight = FontWeight.SemiBold,
+                        color = MaterialTheme.colorScheme.secondary
+                    )
+
+                    Text(
+                        text = state.transaction.dateTime,
+                        fontSize = TextUnit(value = 16f, type = TextUnitType.Sp),
+                        fontWeight = FontWeight.Normal,
+                        color = MaterialTheme.colorScheme.secondary
+                    )
+                }
             }
         }
     }
