83c1c27d907868c9765ec32281f79a9c6d6c93d5
ilkek <ilkerozguraslan@gmail.com>
2026-03-14 17:37:20 +0100

    chore: bump version to 1.0.8

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "me.ilker.balance_tracker"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.0.8"
     }
     packaging {
         resources {
