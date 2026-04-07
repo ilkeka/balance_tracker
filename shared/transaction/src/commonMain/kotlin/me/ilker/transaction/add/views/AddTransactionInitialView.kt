ef87cb650b92e9011dbce7efc8f6c1d80c8d2a9d
ilkek <ilkerozguraslan@gmail.com>
2026-04-07 14:34:04 +0200

    fix: remove unused imports

--- shared/transaction/src/commonMain/kotlin/me/ilker/transaction/add/views/AddTransactionInitialView.kt
+++ shared/transaction/src/commonMain/kotlin/me/ilker/transaction/add/views/AddTransactionInitialView.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -35,7 +34,6 @@ import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.VerticalDivider
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
